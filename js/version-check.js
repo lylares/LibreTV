bb921dedf418eb754ad128e7c219470490416073
lylares <lylares@users.noreply.github.com>
2025-08-18 20:29:25 +0800

    Update version-check.js

--- js/version-check.js
+++ js/version-check.js
@@ -37,8 +37,10 @@ async function checkForUpdates() {
         // 获取最新版本
         let latestVersion;
         const VERSION_URL = {
-            PROXY: 'https://ghfast.top/raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt',
-            DIRECT: 'https://raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt'
+            //PROXY: 'https://ghfast.top/raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt',
+            PROXY:'',
+            DIRECT:''
+            //DIRECT: 'https://raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt'
         };
         const FETCH_TIMEOUT = 1500;
         
